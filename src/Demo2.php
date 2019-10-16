3b8c5c6e1f913820337bcefb5fb9ac67c632ccd7
songzw <songzw1024@163.com>
2019-10-16 10:10:16 +0000

    init

---
+++ src/Demo2.php
@@ -0,0 +1,10 @@
+<?php
+namespace Songzw\Demo2;
+
+class Demo2
+{
+	public function test()
+{
+	echo "I am from Demo2 test...\n";
+}
+}
---
+++ src/Models/User.php
@@ -0,0 +1,11 @@
+<?php
+namespace Songzw\Demo2\Models;
+
+class User
+{
+	public function test()
+
+{
+	echo "I am from user test...\n";
+}
+}
